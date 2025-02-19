65fba47fb162b7763960dd49c0446cdc23d001ab
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-02-20 02:44:18 +0530

    Update dbConn.js

--- configs/dbConn.js
+++ configs/dbConn.js
@@ -12,7 +12,7 @@ mongoose.set("strictQuery", false);
 const connectToDB = async () => {
   try {
     const { connection } = await mongoose.connect(
-      process.env.MONGODB_URI ;
+      process.env.MONGODB_URI
     );
 
     if (connection) {

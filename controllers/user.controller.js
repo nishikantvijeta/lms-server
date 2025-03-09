866c2df58efd0f853af64ae802a390f776855ce8
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-03-10 00:45:35 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,7 +12,7 @@ const cookieOptions = {
   secure: process.env.NODE_ENV === 'production' ? true : false,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
 };
 
 /**
